1fee807dc42390108c363c24df13702d8184d4e5
Thales Ernane <talesernani@gmail.com>
2026-05-18 19:02:16 -0300

    fix: corrigir URL do endpoint pedidos e erro de tipo no template

--- src/app/pedidos/modal-cancelar-pedido/modal-cancelar-pedido.component.ts
+++ src/app/pedidos/modal-cancelar-pedido/modal-cancelar-pedido.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-modal-cancelar-pedido',
@@ -9,6 +9,7 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class ModalCancelarPedidoComponent {
   constructor(
     private dialogRef: MatDialogRef<ModalCancelarPedidoComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
 
   onCloseClick(): void {
--- src/app/pedidos/pedidos.component.ts
+++ src/app/pedidos/pedidos.component.ts
@@ -1,30 +1,40 @@
-import { Component } from '@angular/core';
-import { ModalCancelarPedidoComponent } from './modal-cancelar-pedido/modal-cancelar-pedido.component';
-import { MatDialog } from '@angular/material/dialog';
-import { Observable, of } from 'rxjs';
-import { Reserva } from '../core/types/type';
 import { PedidosService } from './pedidos.service';
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalCancelarPedidoComponent } from './modal-cancelar-pedido/modal-cancelar-pedido.component';
+import { Reserva } from '../core/types/type';
+import { Observable } from 'rxjs';
+import { MensagemService } from '../core/services/mesagem.service';
+
 @Component({
   selector: 'app-pedidos',
   templateUrl: './pedidos.component.html',
   styleUrls: ['./pedidos.component.scss']
 })
 export class PedidosComponent {
+
   pedidos$?: Observable<Reserva[]> = this.pedidosService.getPedidos();
 
   constructor(
     private dialog: MatDialog,
-    private pedidosService: PedidosService
-  ) { }
+    private pedidosService: PedidosService,
+    private messagemService: MensagemService
+  ) {}
 
-  onCancelarPedido(id: number): void {
+  onCancelarPedido(id: number | undefined): void {
     const dialogRef = this.dialog.open(ModalCancelarPedidoComponent, {
       width: '400px',
-      data: { id }
+      data: {id}
     });
 
-    dialogRef.afterClosed().subscribe(() => {
-      console.log('the dialog was closed');
+    dialogRef.afterClosed().subscribe(id => {
+      if (id) {
+        this.pedidosService.removerPedido(id)
+          .subscribe(() => {
+            this.messagemService.openMessage('Pedido removido com sucesso!');
+          });
+      }
     });
   }
+
 }
